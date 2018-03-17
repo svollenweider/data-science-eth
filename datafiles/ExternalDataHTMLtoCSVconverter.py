960037566bc247b35c40b70057f8d349422d6d5e
Silvan Vollenweider <silvan.vollenweider@outlook.com>
2018-03-17 18:02:54 +0100

    Renamed Files, added Verkehrszählung which compresses the bicylce and pedestrian data

---
+++ datafiles/ExternalDataHTMLtoCSVconverter.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+Year = input("Enter Year: ")
+
+Dates = {'2017' : {'dates':['2017-01-01','2017-04-14','2017-04-16','2017-04-17','2017-05-01','2017-05-25','2017-06-05','2017-08-01','2017-12-25','2017-12-26']},
+         '2016' : {'dates':['2016-01-01','2016-03-25','2016-03-27','2016-03-28','2016-05-01','2016-05-25','2016-05-16','2016-08-01','2016-12-25','2016-12-26']},
+         '2015' : {'dates':['2015-01-01','2017-04-14','2017-04-16','2017-04-17','2017-05-01','2017-05-25','2017-06-05','2017-08-01','2017-12-25','2017-12-26']},
+         '2014' : {'dates':['2017-01-01','2017-04-14','2017-04-16','2017-04-17','2017-05-01','2017-05-25','2017-06-05','2017-08-01','2017-12-25','2017-12-26']}}
+
+Specialdays = pd.DataFrame(Dates[Year])
+Specialdays['dates'] = pd.to_datetime(Specialdays['dates'])  
+    
+df = pd.read_html("Weather"+Year+".html",header=0)[0]
+
+DataColumnNamesTemp = ['Datum Uhrzeit','Lufttemperatur','Windgeschwindigkeit','Windrichtung','Luftdruck','Niederschlag','Luftfeuchte']
+
+df.columns = DataColumnNamesTemp
+
+df['Datum'], df['Uhrzeit'] = df['Datum Uhrzeit'].str.split(' ', 1).str
+
+df['Uhrzeit'] = pd.to_timedelta(df['Uhrzeit']).dt.seconds
+
+df['Datum Uhrzeit'] = pd.to_datetime(df['Datum Uhrzeit'])
+
+df['Datum'] = pd.to_datetime(df['Datum'])
+
+df['Weekday'] = df['Datum'].dt.dayofweek
+
+df['Specialday'] = df['Datum'].isin(Specialdays['dates']).astype(int)
+
+df['Datum'] = pd.to_timedelta(df['Datum']-df['Datum'][0])
+
+df['Days'] = (df['Datum']/df['Datum'].iloc[-1]).round(decimals=2)
+
+ColumnNames = ['Datum Uhrzeit','Days','Uhrzeit', 'Weekday', 'Specialday', 'Lufttemperatur', 'Windgeschwindigkeit','Windrichtung', 'Luftdruck', 'Niederschlag', 'Luftfeuchte']
+
+df = df[ColumnNames]
+
+df.to_csv("ExternalData"+Year+"final.csv",index=False)
