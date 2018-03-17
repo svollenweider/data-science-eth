f10c41472c8be2348fa704f0fec0840d9c150d74
Silvan Vollenweider <silvan.vollenweider@outlook.com>
2018-03-17 22:53:17 +0100

    AddedRetriever and TramDataCompressor

---
+++ datafiles/Retriever.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import urllib.request as url
+import re
+import os
+ 
+website = "https://data.stadt-zuerich.ch/dataset/vbz_fahrzeiten_ogd_2017"
+
+html_page = url.urlopen(website)
+soup = BeautifulSoup(html_page)
+links = []
+ 
+for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
+    name = link.get('href')
+    if 'fahrzeitensollist' in name and name.endswith('.csv'):
+        links.append(link.get('href'))
+
+i = 0
+print(str(round(i/len(links),3)*100)+' %')
+for link in links:
+    url.urlretrieve(link, 'SollIst/'+os.path.split(link)[1])
+    i += 1
+    print(str(round(i/len(links),3)*100)+' %')
---
+++ datafiles/TramDataCompressor.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import os
+
+files = [f for f in os.listdir('SollIst') if f.startswith('fahrzeit')]
+seriestodrop = ['datum_von','datum_nach','halt_punkt_id_nach','seq_nach','halt_diva_nach','halt_punkt_diva_nach'
+                ,'soll_an_nach','soll_ab_nach','ist_ab_nach','fahrt_id','fw_no','fw_kurz','umlauf_von'
+                ,'halt_id_nach','halt_punkt_diva_von', 'soll_an_von' , 'ist_an_von','halt_kurz_nach1','ist_an_nach1'
+                ,'fahrweg_id','halt_punkt_id_von','halt_diva_von','seq_von','kurs']
+Dataframes = []
+
+print("{: 6.1f}".format(0)+' %')
+for idx,file in enumerate(files):
+    df = pd.read_csv('SollIST/'+file)
+    df = df.drop(seriestodrop,axis=1)
+    df = df[df.linie == 9].drop('linie',axis=1)
+    df['Delay'] = df.ist_ab_von - df.soll_ab_von
+    df.soll_ab_von = pd.to_timedelta(df.soll_ab_von,unit='s')
+    df.betriebsdatum = pd.to_datetime(df.betriebsdatum) + df.soll_ab_von
+    df = df.drop(['soll_ab_von','ist_ab_von'],axis=1)
+    Dataframes.append(df)
+    print("{: 6.1f}".format((idx+1)/len(files)*100)+' %')
+
+print('Concat now')
+AllData = pd.concat(Dataframes)
+AllData = AllData.sort_values('betriebsdatum').reset_index()
+AllData = AllData.rename(columns={'betriebsdatum' : 'departure'})
+print('Concat finished. Saving')
+AllData.to_csv("VBZDataLine9"+Year+"final.csv",index=False)
+print('Saving successful')
