60d874f3589dc37b2ecbc32590b087729a55b80a
Silvan Vollenweider <silvan.vollenweider@outlook.com>
2018-04-26 13:10:54 +0200

    Put everything into neural network, can now read images from disk, still not working error: "X must be in the same graph as Y" when convoluting

--- neuralnetwork/DataSetCreator.py
+++ neuralnetwork/DataSetCreator.py
@@ -6,23 +6,18 @@ import numpy
 def mappingfunction(feature,label):
     print(feature['Filename'])
     image = tf.read_file(feature['Filename'])
-    image = tf.image.decode_image(image, [36,60])
-    features['image'] = image
-    return features, label
+    image = tf.image.decode_image(image)
+    feature['image'] = image
+    return feature, label
 
 
 def train_input_fn(features, labels, batch_size):
     """An input function for training"""
     # Convert the inputs to a Dataset.
     dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
-
     # Shuffle, repeat, and batch the examples.
-    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
     dataset = dataset.map(mappingfunction)
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    #
     # Return the dataset
-    return dataset
-
-if __name__ = "__main__":
-    Dataframe = pd.read_csv("TraingDatafinal.csv",header=0)[Listoffeatures+['label']]
-    Dataframe = pd.read_csv("TraingDatafinal.csv",header=0)
-    Test = train_input_fn(Dataframe.drop(["label"],axis=1),Dataframe["label"],200)
+    return dataset.make_one_shot_iterator().get_next()
--- neuralnetwork/NeuralNetwork.py
+++ neuralnetwork/NeuralNetwork.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -5,6 +9,24 @@ import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.INFO)
 
 imagesize = 12
+
+def mappingfunction(feature,label):
+    image = tf.read_file('Images/'+feature['Filename'])
+    image = tf.image.decode_jpeg(image,channels=1)
+    image = tf.image.resize_images(image, [36,60])
+    feature['image'] = image
+    return feature, label
+
+
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.map(mappingfunction)
+    dataset = dataset.shuffle(10).repeat().batch(batch_size)
+    # Return the dataset
+    return dataset.make_one_shot_iterator().get_next
 
 def cnn_model(features,labels,mode):
     # Extract Images
@@ -14,31 +36,40 @@ def cnn_model(features,labels,mode):
     # Reshape X to 4-D tensor: [batch_size, width, height, channels]
   
     #Put all images into variables with proper size
-    
-    pedestrian = tf.reshape(features['pedestrian'], [-1, 3*imagesize, 3*imagesize, 1])
-    cyclist = tf.reshape(features['cyclist'], [-1, 2*imagesize, 2*imagesize, 1])
-    pedestrianc = tf.reshape(features['pedestrianc'], [-1, imagesize, imagesize, 1])
-    cyclistc = tf.reshape(features['cyclistc'], [-1, imagesize, imagesize, 1])
+    image = features.pop("image")
+    image = tf.reshape(image,[-1,36,60,1])
+    pedestrian = image[:,0:36,0:36]
+    cyclist = image[:,0:24,36:60]
+    pedestrianc = image[:,24:36,36:48]
+    cyclistc = image[:,24:36,48:60]
 
-    List = [pedestrian,cyclist,pedestrainc,cyclistc]
+    List = [pedestrian,cyclist,pedestrianc,cyclistc]
     Sizes = [3,2,1,1]
+    
+    conv1 = tf.layers.conv2d(
+        inputs=image,
+        filters=16,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+    
     
     inputlayer = []
     
     # For each subimage do X to create input layer
-    for idx,size,element in enumerate(zip(Sizes,List))
+    for idx,zipped in enumerate(zip(Sizes,List)):
         # Convolutional Layer #1
         # Computes 32 features using a 5x5 filter with ReLU activation.
         # Padding is added to preserve width and height.
         # Input Tensor Shape: [batch_size, input size, input size, 1]
         # Output Tensor Shape: [batch_size, input size, input size, 16]
+        size,element = zipped
         conv1 = tf.layers.conv2d(
           inputs=element,
           filters=16,
           kernel_size=[5, 5],
           padding="same",
           activation=tf.nn.relu)
-          
         # Pooling Layer #1
         # First max pooling layer with a 2x2 filter and stride of 3
         # Input Tensor Shape: [batch_size,  input size,  input size, 16]
@@ -62,15 +93,13 @@ def cnn_model(features,labels,mode):
         # Input Tensor Shape: [batch_size, 14, 14, 32]
         # Output Tensor Shape: [batch_size, 7, 7, 32]
         pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-
         # Flatten tensor into a batch of vectors
         # Input Tensor Shape: [batch_size, 7, 7, 64]
         # Output Tensor Shape: [batch_size, 7 * 7 * 64]
-        pool2_flat = tf.reshape(pool2, [-1,pool2.shape[1]*pool2.shape[2]*pool2.shape[3])
+        pool2_flat = tf.reshape(pool2, [-1,pool2.shape[1]*pool2.shape[2]*pool2.shape[3]])
         inputlayer = tf.concat([inputlayer,pool2_flat])
-    inputlayer = tf.concat([inputlayer,features['data'])
+    inputlayer = tf.concat([inputlayer,features])
     
-
     # Dense Layer
     # Densely connected layer with 1024 neurons
     # Input Tensor Shape: [batch_size, 7 * 7 * 64]
@@ -90,15 +119,10 @@ def cnn_model(features,labels,mode):
     # Logits layer
     # Input Tensor Shape: [batch_size, 1024]
     # Output Tensor Shape: [batch_size, 10]
-    logits = tf.layers.dense(inputs=dropout2, units=7)
-
-    predictions = {
-      # Generate predictions (for PREDICT and EVAL mode)
-      "classes": tf.argmax(input=logits, axis=1),
-      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-      # `logging_hook`.
-      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
+    logits = tf.layers.dense(inputs=dropout2, units=1)
+    delay = tf.nn.softmax(logits, name="softmax_tensor") 
+    predictions = {"delay": delay}
+    
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
@@ -106,8 +130,11 @@ def cnn_model(features,labels,mode):
     #loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
     
     #loss for floating output
+    def reducedelay(x):
+        return 1./tf.exp(-(x-100)/50)
+        
+    loss = tf.reduce_mean(tf.square(reducedelay(labels) - delay))
     
-    loss = tf.reduce_mean(tf.square(expected - net))
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
@@ -117,13 +144,23 @@ def cnn_model(features,labels,mode):
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
     # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = {
-      "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
     return tf.estimator.EstimatorSpec(
-    mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    mode=mode, loss=loss)
+
+def main(unused_argv):
+    # Load training and eval data
+    Dataframe = pd.read_csv("TraingDatafinal.csv",header=0)
+    dataset = train_input_fn(Dataframe.drop(["label"],axis=1),Dataframe["label"],10)
+    tram2late = tf.estimator.Estimator(model_fn=cnn_model, model_dir="/snapshots")
+    tensors_to_log = {"delays": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
+    tram2late.train(
+      input_fn=dataset,
+      steps=20000,
+      hooks=[logging_hook])
     
-    
- 
+if __name__ == "__main__":
+    sess = tf.InteractiveSession()
+    tf.app.run()
  
     
